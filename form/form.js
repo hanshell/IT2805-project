2bd31fd14a98424aa9914b0287b988f0f77ecced
Hans Melby <hanshell@stud.ntnu.no>
2014-11-14 16:09:48 +0100

    form javascript added to correct folder

---
+++ form/form.js
@@ -0,0 +1,10 @@
+var clear=true;
+function clearContents(element){
+	if(clear){
+		element.value='';
+		clear=false;
+	}
+}
+document.getElementById('movieSubmit').addEventListener('click', function(){
+	
+})
