8bda96c15005969cb2363c843f3f8898889682cc
Ethol <tomeivin@sutd.ntnu.no>
2014-11-15 15:09:02 +0100

    hei og hå nå funker select from review

--- form/movie_xml.php
+++ form/movie_xml.php
@@ -1,31 +1,52 @@
-<?php 
-	$servername = "mysql.stud.ntnu.no";
-	$username = "tomeivin_webtec";
-	$password = "Doge69";
-	$dbname = "test";
+<!DOCTYPE html>
+<html>
 
-	$connection = new mysqli($servername, $username, $password, $test);
-	if($connection->connect_error){
-		die("Connection failed" . mysqli_connect_error());
-	}
+<head>
+<meta charset="UTF-8">
+
+<title>Movie review</title>
+</head>
+<body>
+
+<?php
+	// Connecting, selecting database
+	$link = mysql_connect('mysql.stud.ntnu.no', 'tomeivin_webtec', 'Doge69')
+	    or die('Could not connect: ' . mysql_error());
+	/*echo 'Connected successfully';*/
+	mysql_select_db('test') or die('Could not select database');
+
+	// Performing SQL query
+
 
 	$movie_title=$_GET['title'];
 
 	echo $movie_title;
 
-	$sql="SELECT * FROM test.movie WHERE title='" . $movie_title . "'"; 
-	if($connection->query($sql)===TRUE){
-		echo "RECORD CREATED SUCCESSFULLY";
+	$query="SELECT * FROM movie WHERE title='" . $movie_title . "'"; 
+	$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+
+	// Printing results in HTML
+	echo "<table>\n";
+	while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+	    echo "\t<tr>\n";
+	    foreach ($line as $col_value) {
+	        echo "\t\t<td>$col_value</td>\n";
+	    }
+	    echo "\t</tr>\n";
 	}
-	else{
-		echo "ERROR" . $sql . $connection->error;
-	}
+	echo "</table>\n";
 
 
-	
-	header("Location: index.php");
+	echo $result;
+	// Free resultset
+	mysql_free_result($result);
 
-	mysql_close($connection);
-
-
+	// Closing connection
+	mysql_close($link);
 ?>
+
+
+
+</body>
+
+</html>
