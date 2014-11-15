fa8ba2f76f80c69bc96f8a3147ba8218e2d6838d
Hans Melby <hanshell@stud.ntnu.no>
2014-11-15 16:52:09 +0100

    xml generation FINALLY works...

--- form/movie_xml.php
+++ form/movie_xml.php
@@ -1,52 +1,56 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-<meta charset="UTF-8">
-
-<title>Movie review</title>
-</head>
-<body>
-
 <?php
 	// Connecting, selecting database
 	$link = mysql_connect('mysql.stud.ntnu.no', 'tomeivin_webtec', 'Doge69')
 	    or die('Could not connect: ' . mysql_error());
 	/*echo 'Connected successfully';*/
-	mysql_select_db('test') or die('Could not select database');
+	mysql_select_db('test') 
+		or die('Could not select database');
 
 	// Performing SQL query
 
 
-	$movie_title=$_GET['title'];
+//	$movie_title=$_GET['title'];
 
-	echo $movie_title;
+	$movie_title="The Room";
+
 
 	$query="SELECT * FROM movie WHERE title='" . $movie_title . "'"; 
 	$result = mysql_query($query) or die('Query failed: ' . mysql_error());
 
+
 	// Printing results in HTML
-	echo "<table>\n";
+	$movie_array=array();
 	while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
-	    echo "\t<tr>\n";
+//	    echo "\t<tr>\n";
 	    foreach ($line as $col_value) {
-	        echo "\t\t<td>$col_value</td>\n";
+//	        echo $col_value . "<br>";
+	        $movie_array[]=$col_value;
 	    }
-	    echo "\t</tr>\n";
 	}
-	echo "</table>\n";
+	foreach ($movie_array as $column) {
+//		echo $column . "<br>";
+	}
 
+	$xml=new SimpleXMLElement("<xml></xml>");
 
-	echo $result;
+	$movie=$xml->addChild('movie');
+	
+	$title=$movie->addChild('title', $movie_array[0]);
+	$description=$movie->addChild('description', $movie_array[1]);
+	$director=$movie->addChild('director', $movie_array[2]);
+	$genre=$movie->addChild('genre', $movie_array[3]);
+/*
+	$title->addChild($movie_array[0]);
+	$description->addChild($movie_array[1]);
+	$director->addChild($movie_array[2]);
+	$genre->addChild($movie_array[3]);
+*/
+	Header('Content-type: text/xml');
+	echo $xml->asXML();
+
 	// Free resultset
 	mysql_free_result($result);
 
 	// Closing connection
 	mysql_close($link);
 ?>
-
-
-
-</body>
-
-</html>
