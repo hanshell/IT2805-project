f8bed98a06c6f86817ebce33214ec256a5b0e956
=Luan Tran <tuonglua@stud.ntnu.no>
2014-11-26 18:03:46 +0100

    Add footer

--- index.php
+++ index.php
@@ -10,42 +10,48 @@
 </head>
 
 <body>
+<div id="wrapper">
 <img src="logo.png" width="50%" alt="logo"/>
 <?php
 	include "menubar.php";
 	echo generate(); 
 ?>
 
+<div id="container">
+	<?php
+	// Connecting, selecting database
+	$link = mysql_connect('mysql.stud.ntnu.no', 'tomeivin_web', '1337Doge')
+	    or die('Could not connect: ' . mysql_error());
+	/*echo 'Connected successfully';*/
+	mysql_select_db('tomeivin_review') or die('Could not select database');
 
-<?php
-// Connecting, selecting database
-$link = mysql_connect('mysql.stud.ntnu.no', 'tomeivin_web', '1337Doge')
-    or die('Could not connect: ' . mysql_error());
-/*echo 'Connected successfully';*/
-mysql_select_db('tomeivin_review') or die('Could not select database');
+	// Performing SQL query
+	$query = 'SELECT * FROM review';
+	$result = mysql_query($query) or die('Query failed: ' . mysql_error());
 
-// Performing SQL query
-$query = 'SELECT * FROM review';
-$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+	// Printing results in HTML
+	echo "<table>\n";
+	while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+	    echo "\t<tr>\n";
+	    foreach ($line as $col_value) {
+	        echo "\t\t<td>$col_value</td>\n";
+	    }
+	    echo "\t</tr>\n";
+	}
+	echo "</table>\n";
 
-// Printing results in HTML
-echo "<table>\n";
-while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
-    echo "\t<tr>\n";
-    foreach ($line as $col_value) {
-        echo "\t\t<td>$col_value</td>\n";
-    }
-    echo "\t</tr>\n";
-}
-echo "</table>\n";
+	// Free resultset
+	mysql_free_result($result);
 
-// Free resultset
-mysql_free_result($result);
+	// Closing connection
+	mysql_close($link);
+	?>
+</div>
 
-// Closing connection
-mysql_close($link);
-?>
-
+<div id="author">
+        Author: Luan Tran
+</div>
+</div>
 </body>
 
 </html>
